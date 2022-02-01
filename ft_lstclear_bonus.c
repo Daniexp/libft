38e47624a66e5481d8da38acb695f8a7600e9352
Daniel Exposito lopez <dexposit@c2r17s3.42madrid.com>
2022-02-01 19:20:37 +0100

    bonusname

--- ft_lstclear_bonus.c
+++ ft_lstclear_bonus.c
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_lstclear.c                                      :+:      :+:    :+:   */
+/*   ft_lstclear_bonus.c                                :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: dexposit <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/02/01 13:27:55 by dexposit          #+#    #+#             */
-/*   Updated: 2022/02/01 18:17:49 by dexposit         ###   ########.fr       */
+/*   Created: 2022/02/01 18:40:18 by dexposit          #+#    #+#             */
+/*   Updated: 2022/02/01 19:16:26 by dexposit         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,5 +14,16 @@
 
 void	ft_lstclear(t_list **lst, void (*del)(void*))
 {
+	t_list	*prev;
 
+	if (!lst)
+		return ;
+	while (*lst)
+	{
+		(*del)((*lst)->content);
+		prev = *lst;
+		*lst = (*lst)->next;
+		free(prev);
+	}
+	*lst = 0;
 }
